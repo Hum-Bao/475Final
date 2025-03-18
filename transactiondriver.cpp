f17cb96271c35b548151ebef5e3054b052eb2b8f
acheema5 <166875663+acheema5@users.noreply.github.com>
2025-03-17 21:56:12 -0700

    Update transactiondriver.cpp

--- transactiondriver.cpp
+++ transactiondriver.cpp
@@ -1,1 +1,15 @@
+#include "transactiondriver.h"
+#include <iostream>
+#include <string>
+#include "SQLAPI/include/SQLAPI.h"
+#include "transaction.h"
 
+const std::array<std::pair<int, std::string>, CustomerDriver::NUM_METHODS>
+    CustomerDriver::METHODS = {{{1, "CreateTransaction"},
+                                {2, "GetTransactionByCustomer"},
+                                {3, "GetTransactionByFacility"},
+                                {4, "GetTransactionByDateRange"},
+                                {5, "Return"}}};
+
+const std::array<std::pair<int, std::string>, CustomerDriver::NUM_FIELDS>
+    CustomerDriver::FIELDS = {{{1, "TYPE"}, {2, "TIME"}, {3, "SHIPPING"}, {4, "FACILITY"}, {5, "CUSTOMER"} {6, "NUMBER"}}};
