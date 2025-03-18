b8fbe4ba1762e3bc38db8e204c1faeb79e144bfb
acheema5 <166875663+acheema5@users.noreply.github.com>
2025-03-17 18:20:47 -0700

    Update facility and ListAllFacilities

--- facility.cpp
+++ facility.cpp
@@ -1,7 +1,6 @@
 #include "facility.h"
 #include <iostream>
 #include "SQLAPI/include/SQLAPI.h"
-#include <iostream>
 
 void Facility::CreateFacility(SAConnection& con, std::string name, std::string region){
       SACommand insert(
@@ -11,13 +10,42 @@ void Facility::CreateFacility(SAConnection& con, std::string name, std::string r
         insert << _TSA(name.c_str()) << _TSA(reigon.c_str());
         insert.Execute();
         //for testing purposes|| where will we actually add the success conformation messages???
-        cout << "facility successfully created" << endl; 
+        std::cout << "facility successfully created" << endl; 
 }
 
 void Facility::UpdateFacility(SAConnection& con, std::string name, std::string update_field){
+      SACommand select(&con, _TSA("SELECT ID FROM FACILITY WHERE NAME = :1"));
+      select << _TSA(name.c_str());
+      select.Execute();
+
+    if (select.FetchNext()) {
+        int id = select.Field(1).asLong();
+
+
+        std::string command =
+            "UPDATE FACILITY SET " + update_field + "= :1 WHERE ID = :2";
+        SACommand update(&con, _TSA(command.c_str()));
+        update << _TSA(new_val.c_str()) << id;
+        update.Execute();
+    } else {
+        std::cerr << "Facility with name: " << name << " not found.\n";
+    }
   
 }
 
 void Facility::ListAllFacilities(SAConnection& con){
-  
+      SACommand select(&con, _TSA("SELECT * FROM FACILITY"));
+      select.Execute();
+
+      SAString select_name;
+      SAString select_reigon;
+      std::cout << "Showing " << select.RowsAffected() << " facilities\n";
+      while (select.FetchNext()) {
+        //Select[1] is the id
+            select_name = select[2].asString();
+            select_reigon = select[3].asString();
+            std::cout << "Name: " << select_name.GetMultiByteChars() << "\n";
+            std::cout << "  Email: " << select_reigon.GetMultiByteChars() << "\n";
+            std::cout << "\n";
+    }
 }
