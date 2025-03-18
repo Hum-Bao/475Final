fc0474f00363a5f246174948e3f8e6f371c9f45c
acheema5 <166875663+acheema5@users.noreply.github.com>
2025-03-17 17:59:17 -0700

    Create facility.cpp

---
+++ facility.cpp
@@ -0,0 +1,15 @@
+#include "facility.h"
+#include <iostream>
+#include "SQLAPI/include/SQLAPI.h"
+
+void Facility::CreateFacility(SAConnection& con, std::string name, std::string region){
+  
+}
+
+void Facility::UpdateFacility(SAConnection& con, std::string name, std::string update_field){
+  
+}
+
+void Facility::ListAllFacilities(SAConnection& con){
+  
+}
