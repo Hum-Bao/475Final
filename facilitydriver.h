53a67c068308f2b98e05b610a6663fa2d5f4a925
acheema5 <166875663+acheema5@users.noreply.github.com>
2025-03-18 13:20:58 -0700

    Create facilitydriver.h

---
+++ facilitydriver.h
@@ -0,0 +1,19 @@
+#ifndef FACILITYDRIVER_H
+#define FACILITYDRIVER_H
+
+#include <array>
+#include <string>
+#include "SQLAPI/include/SQLAPI.h"
+
+class TransactionDriver {
+    public:
+        static void SelectGoodsAPI(SAConnection& con);
+
+    private:
+        const static int NUM_METHODS = 3;
+        const static int NUM_FIELDS = 2;
+        const static std::array<std::pair<int, std::string>, NUM_METHODS>
+            METHODS;
+        const static std::array<std::pair<int, std::string>, NUM_FIELDS> FIELDS;
+};
+#endif
