7294f8aa3ed78beb1a851fcc558d81cb9c5d3315
acheema5 <166875663+acheema5@users.noreply.github.com>
2025-03-17 18:10:59 -0700

    Updated facility insert

--- facility.cpp
+++ facility.cpp
@@ -1,9 +1,17 @@
 #include "facility.h"
 #include <iostream>
 #include "SQLAPI/include/SQLAPI.h"
+#include <iostream>
 
 void Facility::CreateFacility(SAConnection& con, std::string name, std::string region){
-  
+      SACommand insert(
+        &con,
+        _TSA("INSERT INTO Facility (NAME, REIGON) VALUES (:1, :2)"));
+
+        insert << _TSA(name.c_str()) << _TSA(reigon.c_str());
+        insert.Execute();
+        //for testing purposes|| where will we actually add the success conformation messages???
+        cout << "facility successfully created" << endl; 
 }
 
 void Facility::UpdateFacility(SAConnection& con, std::string name, std::string update_field){
